2a2d0eaf2c51bb5279f0989a803ab759657a2ae2
xviceversa95 <a.meshalnikova@yandex.ru>
2025-02-04 22:45:35 +0300

    Add files via upload

--- src/ru/netology/graphics/Main.java
+++ src/ru/netology/graphics/Main.java
@@ -1,6 +1,9 @@
 package ru.netology.graphics;
 
+import ru.netology.graphics.image.Converter;
+import ru.netology.graphics.image.TextColorSchema;
 import ru.netology.graphics.image.TextGraphicsConverter;
+import ru.netology.graphics.image.WhiteColorSchema;
 import ru.netology.graphics.server.GServer;
 
 import java.io.File;
@@ -8,14 +11,15 @@ import java.io.PrintWriter;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        TextGraphicsConverter converter = null; // Создайте тут объект вашего класса конвертера
+        TextGraphicsConverter converter = new Converter();// Создайте тут объект вашего класса конвертера
+
 
         GServer server = new GServer(converter); // Создаём объект сервера
         server.start(); // Запускаем
 
-        // Или то же, но с выводом на экран:
-        //String url = "https://raw.githubusercontent.com/netology-code/java-diplom/main/pics/simple-test.png";
-        //String imgTxt = converter.convert(url);
-        //System.out.println(imgTxt);
+         // Или то же, но с выводом на экран:
+        // String url = "https://i.ibb.co/6DYM05G/edu0.jpg";
+       //String imgTxt = converter.convert(url);
+      //  System.out.println(imgTxt);
     }
 }
